2875e9dcf931413b5e0dc80245e51ce87c62dabd
Erma Zunic <zunicerma@gmail.com>
2024-06-15 15:34:59 +0000

    edditing comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path("detail/<slug:slug>/", views.post_detail, name="post_detail") 
+    path("detail/<slug:slug>/", views.post_detail, name="post_detail"),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
